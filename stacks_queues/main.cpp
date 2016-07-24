cff0775c0c335309971240ad5d3bc44ff3735095
Allison Kane <allison.kane4@gmail.com>
2016-07-24 12:31:21 -0500

    Initial Commit

---
+++ stacks_queues/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stacks_queues
+//
+//  Created by Allison Kane on 7/24/16.
+//  Copyright © 2016 Allison Kane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
